bb1b576bd7a66edc57e64ca60475193db8249f0a
John McCain <johnm.freestate@gmail.com>
2016-02-28 21:42:57 -0600

    Add customerFrontend.html, formChecker.js, and customerBackend.php (customerBackend.php needs to be finished)

---
+++ customerBackend.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Excercise 3: Store</title>
+    <!--Bootstrap CSS-->
+  	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+  	<!--jQuery-->
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+  	<!--Bootstrap JS-->
+  	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+  	<link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <h1>Excercise 3: Store</h1>
+    <br>
+    <br>
+    <?php
+
+     ?>
+  </body>
+</html>
---
+++ formChecker.js
@@ -0,0 +1,43 @@
+function reset()
+{
+  document.getElementById('toteQuantity').value = 0;
+  document.getElementById('backpackQuantity').value = 0;
+  document.getElementById('mugQuantity').value = 0;
+  return false;
+}
+
+function shippingChecker()
+{
+  var ship = document.store.shipping;
+  for(var i=0; i<ship.length; i++)
+  {
+    if(ship[i].checked == true)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+function validate()
+{
+  var toteQuantity = document.getElementById('toteQuantity').value;
+  var backpackQuantity = document.getElementById('backpackQuantity').value;
+  var mugQuantity = document.getElementById('mugQuantity').value;
+  if(toteQuantity < 0 || backpackQuantity < 0 || mugQuantity < 0)
+  {
+    alert('quantities cannot be less than 0');
+    return false;
+  }
+  else if(toteQuantity + backpackQuantity + mugQuantity == 0)
+  {
+    alert('you have to buy SOMETHING :(');
+    return false;
+  }
+  else if(!shippingChecker())
+  {
+    alert('please select a shipping option');
+    return false;
+  }
+  return true;
+}
